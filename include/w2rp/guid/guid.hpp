148d55c4ede827a059560766095f8deceb8b1238
danielt <tappe@ida.ing.tu-bs.de>
2024-05-30 17:51:54 +0200

    added GUID generation/handling classes

--- include/w2rp/guid/guid.hpp
+++ include/w2rp/guid/guid.hpp
@@ -1,6 +1,9 @@
 #ifndef Guid_h
 #define Guid_h
 
+#include <w2rp/guid/guidPrefix.hpp>
+#include <w2rp/guid/entityID.hpp>
+
 #include <string>
 #include <cstdint>
 #include <cstring>
@@ -11,90 +14,53 @@
 namespace w2rp
 {
 
-/********************** General ***************************/
-#define VENDOR_ID 0x01DA
-
-/********************** Entity IDs ***************************/
-#define ENTITYID_READER 0x0010
-#define ENTITYID_WRITER 0x0001
-
-const unsigned char DEFAULT_ID_CHAR[] = "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"; 
-
-const uint8_t VENDOR_ID_LEN = 2;
-const uint8_t HOST_ID_LEN = 2;
-const uint8_t PROCESS_ID_LEN = 8;
-
-const uint8_t VENDOR_ID_OFFSET = 0;
-const uint8_t HOST_ID_OFFSET = 2;
-const uint8_t PROCESS_ID_OFFSET = HOST_ID_OFFSET + HOST_ID_LEN;
-
 /**
  * @brief GUID for applications / entities
- * 
- * Encoding:
- * Bytes 0-1: VendorID
- * Bytes 2-3: HostID
- * Bytes 4-: ProcessID
- * Bytes 12-15: Entity ID (Reader/Writer ...)
- * 
+ *  
  */
 struct GUID_t
 {
-    static constexpr unsigned int size = 16;
-    
-    unsigned char value[size];
-
-    GUID_t()
-    {
-        memset(value, 0xFF, size);
-    };
-
-    GUID_t(unsigned char val[size])
-    {
-        memcpy(value, val, size);
-    }
-
-    GUID_t(const unsigned char val[size])
-    {
-        memcpy(value, val, size);
-    }
-
+    GuidPrefix_t guidPrefix;
+    EntityID_t entityID;
 
     /**
-     * Guid prefix comparison operator
-     * @param entity_id entity_id to compare
-     * @return True if the entity_ids are equal
+     * @brief Default constructor. Contructs an unknown GUID.
      */
-    bool operator ==(
-            const GUID_t& entity_id) const
+    GUID_t() noexcept
     {
-        return (memcmp(value, entity_id.value, size) == 0);
     }
 
+
     /**
-     * Guid prefix comparison operator
-     * @param entity_id Second guid prefix to compare
-     * @return True if the entity_ids are not equal
+     * @brief Construct a new guid t object
+     * 
+     * @param guid_prefix Guid prefix
+     * @param id Entity id
      */
-    bool operator !=(
-            const GUID_t& entity_id) const
+    GUID_t(
+        const GuidPrefix_t& guid_prefix,
+        uint32_t id) noexcept
+        : guidPrefix(guid_prefix)
+        , entityID(id)
     {
-        return (memcmp(value, entity_id.value, size) != 0);
     }
 
     /**
-     * Guid prefix minor operator
-     * @param entity_id Second entity_id to compare
-     * @return True if entity_id is higher
+     * @brief Construct a new guid t object
+     * 
+     * @param guid_prefix Guid prefix
+     * @param entity_id Entity id
      */
-    bool operator <(
-            const GUID_t& entity_id) const
+    GUID_t(
+            const GuidPrefix_t& guid_prefix,
+            const EntityID_t& entity_id) noexcept
+        : guidPrefix(guid_prefix)
+        , entityID(entity_id)
     {
-        return std::memcmp(value, entity_id.value, size) < 0;
     }
 
     /**
-     * Checks whether this guid is for an entity on the same host as another guid.
+     * @brief Checks whether this guid is for an entity on the same host as another guid.
      *
      * @param other_guid GUID_t to compare to.
      *
@@ -103,7 +69,7 @@ struct GUID_t
     bool is_on_same_host_as(
             const GUID_t& other_guid) const
     {
-        return memcmp((value+HOST_ID_OFFSET), (other_guid.value+HOST_ID_OFFSET), VENDOR_ID_LEN) == 0;
+        return memcmp(guidPrefix.value, other_guid.guidPrefix.value, VENDOR_ID_LEN+HOST_ID_LEN) == 0;
     }
 
     /**
@@ -116,116 +82,141 @@ struct GUID_t
     bool is_on_same_process_as(
             const GUID_t& other_guid) const
     {
-        return memcmp((value+PROCESS_ID_OFFSET), (other_guid.value+PROCESS_ID_OFFSET), PROCESS_ID_LEN) == 0;
+        return memcmp(guidPrefix.value, other_guid.guidPrefix.value, VENDOR_ID_LEN+HOST_ID_LEN+PROCESS_ID_LEN) == 0;
     }
 
-    uint16_t get_host()
+    static GUID_t unknown() noexcept
     {
-        return ((static_cast<uint16_t>(value[HOST_ID_OFFSET]) << 8) + static_cast<uint16_t>(value[HOST_ID_OFFSET+HOST_ID_LEN-1]));
-    }
-
-    uint32_t get_prefix()
-    {
-        return(
-            (static_cast<uint32_t>(value[VENDOR_ID_OFFSET] << 24) + 
-            static_cast<uint32_t>(value[VENDOR_ID_OFFSET+VENDOR_ID_LEN-1] << 16) + 
-            static_cast<uint32_t>(value[HOST_ID_OFFSET] << 8) +
-            static_cast<uint32_t>(value[HOST_ID_OFFSET+HOST_ID_LEN-1]))
-        );
-    }
-
-    void overwrite(void* val, unsigned long val_size)
-    { 
-        // Reverse byte order of value so that the LSB of val is at value[15]!
-        uint8_t* p = static_cast<uint8_t*>(val);
-        std::reverse(p, p + val_size);
-        memcpy(value+size-val_size, val, val_size);
-    }
-
-    std::string get_value()
-    {
-        std::stringstream stream;
-        stream << std::hex;
-        for (uint8_t i = 0; i < 15; ++i)
-        {
-            stream << std::setw(1) << (int)value[i];
-        }
-        stream << std::setw(1) << (int)value[15];
-
-        return stream.str();
+        return GUID_t();
     }
 
 };
 
-const GUID_t DEFAULT_ID(DEFAULT_ID_CHAR);
 
 /**
- * @brief Output stream for Application GUID
- * 
- * @param output 
- * @param id 
- * @return std::ostream& 
+ * GUID comparison operator
+ * @param g1 First GUID to compare
+ * @param g2 Second GUID to compare
+ * @return True if equal
  */
-inline std::ostream& operator <<(
-        std::ostream& output,
-        const GUID_t& id)
+inline bool operator ==(
+        const GUID_t& g1,
+        const GUID_t& g2)
 {
-    output << std::hex;
-    char old_fill = output.fill('0');
-    for (uint8_t i = 0; i < 15; ++i)
+    if (g1.guidPrefix == g2.guidPrefix && g1.entityID == g2.entityID)
     {
-        output << std::setw(2) << (int)id.value[i] << ".";
+        return true;
     }
-    output << std::setw(2) << (int)id.value[15];
-    output.fill(old_fill);
-    return output << std::dec;
+    else
+    {
+        return false;
+    }
 }
 
 /**
- * @brief Input stream for Application GUID
- * 
- * @param input 
- * @param id 
- * @return std::istream& 
+ * GUID comparison operator
+ * @param g1 First GUID to compare
+ * @param g2 Second GUID to compare
+ * @return True if not equal
+ */
+inline bool operator !=(
+        const GUID_t& g1,
+        const GUID_t& g2)
+{
+    if (g1.guidPrefix != g2.guidPrefix || g1.entityID != g2.entityID)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+inline bool operator <(
+        const GUID_t& g1,
+        const GUID_t& g2)
+{
+    for (uint8_t i = 0; i < 12; ++i)
+    {
+        if (g1.guidPrefix.value[i] < g2.guidPrefix.value[i])
+        {
+            return true;
+        }
+        else if (g1.guidPrefix.value[i] > g2.guidPrefix.value[i])
+        {
+            return false;
+        }
+    }
+    for (uint8_t i = 0; i < 4; ++i)
+    {
+        if (g1.entityID.value[i] < g2.entityID.value[i])
+        {
+            return true;
+        }
+        else if (g1.entityID.value[i] > g2.entityID.value[i])
+        {
+            return false;
+        }
+    }
+    return false;
+}
+
+const GUID_t Guid_Unknown;
+
+/**
+ * @brief Stream operator, prints a GUID.
+ * @param output Output stream.
+ * @param guid GUID_t to print.
+ * @return Stream operator.
+ */
+inline std::ostream& operator <<(
+        std::ostream& output,
+        const GUID_t& guid)
+{
+    if (guid != Guid_Unknown)
+    {
+        output << guid.guidPrefix << "|" << guid.entityID;
+    }
+    else
+    {
+        output << "|GUID UNKNOWN|";
+    }
+    return output;
+}
+
+/**
+ * Stream operator, retrieves a GUID.
+ * @param input Input stream.
+ * @param guid GUID_t to print.
+ * @return Stream operator.
  */
 inline std::istream& operator >>(
         std::istream& input,
-        GUID_t& id)
+        GUID_t& guid)
 {
     std::istream::sentry s(input);
 
     if (s)
     {
-        char point;
-        unsigned short hex;
         std::ios_base::iostate excp_mask = input.exceptions();
 
         try
         {
             input.exceptions(excp_mask | std::ios_base::failbit | std::ios_base::badbit);
-            input >> std::hex >> hex;
 
-            if (hex > 255)
+            char sep;
+            input >> guid.guidPrefix >> sep >> guid.entityID;
+
+            if (sep != '|')
             {
                 input.setstate(std::ios_base::failbit);
             }
-
-            id.value[0] = static_cast<unsigned char>(hex);
-
-            for (int i = 1; i < 16; ++i)
-            {
-                input >> point >> hex;
-                if ( point != '.' || hex > 255 )
-                {
-                    input.setstate(std::ios_base::failbit);
-                }
-                id.value[i] = static_cast<unsigned char>(hex);
-            }
-
-            input >> std::dec;
         }
-        catch (std::ios_base::failure& )
+        catch (std::ios_base::failure&)
         {
+            // maybe is unknown or just invalid
+            guid = Guid_Unknown;
         }
 
         input.exceptions(excp_mask);
@@ -234,7 +225,6 @@ inline std::istream& operator >>(
     return input;
 }
 
-
-};
+}; // end namespace w2rp
 
 #endif
--- include/w2rp/guid/guidPrefix.hpp
+++ include/w2rp/guid/guidPrefix.hpp
@@ -1,40 +1,164 @@
-#ifndef GUIDPREFIX_h
-#define GUIDPREFIX_h
+#ifndef GuidPrefix_h
+#define GuidPrefix_h
 
+#include <string>
+#include <cstdint>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
 
-namespace w2rp{
-
-class guidPrefix
+namespace w2rp
 {
-    public:
 
-    void create()
+const uint16_t vendorID_IDA = 0x01DA;
+
+const uint8_t VENDOR_ID_LEN = 2;
+const uint8_t HOST_ID_LEN = 4;
+const uint8_t PARTICIPANT_ID_LEN = 2;
+const uint8_t PROCESS_ID_LEN = 4;
+
+const uint8_t VENDOR_ID_OFFSET = 0;
+const uint8_t HOST_ID_OFFSET = 2;
+const uint8_t PARTICIPANT_ID_OFFSET = HOST_ID_OFFSET + HOST_ID_LEN;
+const uint8_t PROCESS_ID_OFFSET = PARTICIPANT_ID_OFFSET + PARTICIPANT_ID_LEN;
+
+/**
+ * @brief GUID prefix for applications / entities
+ * 
+ * Encoding:
+ * Bytes 0-1: VendorID
+ * Bytes 2-9: HostID
+ * Bytes 10-11: ProcessID
+ * 
+ */
+struct GuidPrefix_t
+{
+    static constexpr unsigned int size = 12;
+    
+    unsigned char value[size];
+
+    GuidPrefix_t()
     {
+        memset(value, 0, size);
+    };
 
+    GuidPrefix_t(unsigned char val[size])
+    {
+        memcpy(value, val, size);
+    }
+
+    GuidPrefix_t(const unsigned char val[size])
+    {
+        memcpy(value, val, size);
+    }
+
+    static GuidPrefix_t unknown()
+    {
+        return GuidPrefix_t();
     }
 
     /**
-     * Get a reference to the singleton instance.
-     *
-     * @return reference to the singleton instance.
+     * Guid prefix comparison operator
+     * @param prefix guid prefix to compare
+     * @return True if the guid prefixes are equal
      */
-    static const guidPrefix& instance()
+    bool operator ==(
+            const GuidPrefix_t& prefix) const
     {
-        static guidPrefix singleton;
-        return singleton;
+        return (memcmp(value, prefix.value, size) == 0);
     }
-    
-    private:
 
-    guidPrefix()
+    /**
+     * Guid prefix comparison operator
+     * @param prefix Second guid prefix to compare
+     * @return True if the guid prefixes are not equal
+     */
+    bool operator !=(
+            const GuidPrefix_t& prefix) const
     {
-
+        return (memcmp(value, prefix.value, size) != 0);
     }
-    
 
+    /**
+     * Guid prefix minor operator
+     * @param prefix Second guid prefix to compare
+     * @return True if prefix is higher
+     */
+    bool operator <(
+            const GuidPrefix_t& prefix) const
+    {
+        return std::memcmp(value, prefix.value, size) < 0;
+    }
 };
 
+/**
+ * @brief define default "Unknown" guid prefix
+ * 
+ */
+const GuidPrefix_t GuidPrefix_Unknown;
 
-} // end namespace w2rp
+inline std::ostream& operator <<(
+        std::ostream& output,
+        const GuidPrefix_t& guiP)
+{
+    output << std::hex;
+    char old_fill = output.fill('0');
+    for (uint8_t i = 0; i < 11; ++i)
+    {
+        output << std::setw(2) << (int)guiP.value[i] << ".";
+    }
+    output << std::setw(2) << (int)guiP.value[11];
+    output.fill(old_fill);
+    return output << std::dec;
+}
+
+inline std::istream& operator >>(
+        std::istream& input,
+        GuidPrefix_t& guiP)
+{
+    std::istream::sentry s(input);
+
+    if (s)
+    {
+        char point;
+        unsigned short hex;
+        std::ios_base::iostate excp_mask = input.exceptions();
+
+        try
+        {
+            input.exceptions(excp_mask | std::ios_base::failbit | std::ios_base::badbit);
+            input >> std::hex >> hex;
+
+            if (hex > 255)
+            {
+                input.setstate(std::ios_base::failbit);
+            }
+
+            guiP.value[0] = static_cast<unsigned char>(hex);
+
+            for (int i = 1; i < 12; ++i)
+            {
+                input >> point >> hex;
+                if ( point != '.' || hex > 255 )
+                {
+                    input.setstate(std::ios_base::failbit);
+                }
+                guiP.value[i] = static_cast<unsigned char>(hex);
+            }
+
+            input >> std::dec;
+        }
+        catch (std::ios_base::failure& )
+        {
+        }
+
+        input.exceptions(excp_mask);
+    }
+
+    return input;
+}
+   
+}; // end namespace w2rp
 
 #endif
